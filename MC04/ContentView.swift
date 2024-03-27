dd46c0b0c2c11270f022f66e2f55f1ad034ea013
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-03-27 14:14:19 -0300

    Initial Commit

---
+++ MC04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC04
+//
+//  Created by Laura C. Balbachan dos Santos on 27/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC04/MC04App.swift
@@ -0,0 +1,17 @@
+//
+//  MC04App.swift
+//  MC04
+//
+//  Created by Laura C. Balbachan dos Santos on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MC04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
