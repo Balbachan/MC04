b2ad397417092b80de01070abd9fe1c3adf7680d
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-03-28 13:53:19 -0300

    Adicionar as pastas MVVM e pasta de Componentes

---
+++ MC04/Model/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  MC04
+//
+//  Created by Laura C. Balbachan dos Santos on 27/03/24.
+//
+
+//  No Model fica toda a parte dos dados e lógica
+//  O Model só se comunica com o View Model
+import Foundation
