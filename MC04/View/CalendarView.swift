5de0f64e8fbee00aae9e3ea54070c711bf50a1ef
bia-andreucci02 <124587486+bia-andreucci02@users.noreply.github.com>
2024-04-04 14:15:08 -0300

    arrumar as previews

--- MC04/View/CalendarView.swift
+++ MC04/View/CalendarView.swift
@@ -100,7 +100,7 @@ struct CalendarView: View {
     do {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Habits.self, configurations: config)
-        let example = Habits(id: UUID(), name: "hoje", isDone: true, desc: "sdv", steps: "sdv", images: "sdv", startDate: Date(), finalDate: Date(), daysOfWeek: [1], time: Date())
+        let example = Habits(id: UUID(), name: "Lavar o rosto", isDone: true, desc: "Indicado de manhã e a noite.Passo essencial para limpar a pele, serve para remover a oleosidade e impurezas.Não esqueça de escolher um sabonete adequado para seu tipo de pele.", steps: [["1","Lave suas mãos", "Antes de começar a lavar o rosto lave suas mãos. Assim você não vai contagiar seu rosto com possíveis bacterias."],["2", "Use água morna", "Cuidado com a temperatura da água sempre tente lavar o rosto com uma água que esteja morna. Água muito quente pode causar danos a pele."]], images: "sdv", startDate: Date(), finalDate: Date(), daysOfWeek: [1], time: Date())
         return CalendarView()
             .modelContainer(container)
     } catch {
