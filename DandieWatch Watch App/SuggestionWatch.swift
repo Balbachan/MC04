e09267f9f9788f97b344e355b3f38e4a3d494bc2
bia-andreucci02 <124587486+bia-andreucci02@users.noreply.github.com>
2024-04-11 18:07:07 -0300

    adicionar o watch

---
+++ DandieWatch Watch App/SuggestionWatch.swift
@@ -0,0 +1,59 @@
+//
+//  SuggestionsView.swift
+//  MC04
+//
+//  Created by Beatriz Andreucci on 03/04/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct SuggestionWatch: View {
+    var viewModel: ViewModel = ViewModel()
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
+    
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                HStack {
+                    Text("Hábitos")
+                        .font(.custom(FontType.t1.font, size: 28))
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                
+                VStack(alignment: .center) {
+                    ForEach(viewModel.habits) { habit in
+                     /*   NavigationLink(destination: EditTaskView(habitModel: habit)) *//*{*/
+                            Text("\(habit.name)")
+                        }
+//                        .buttonStyle(SuggestionButtonStyle())
+                        .padding(.top, 10)
+                    }
+                    
+                }
+                
+                NavigationLink {
+                    Text("aaa")
+                } label: {
+                    Text("Concluir rotina")
+                }
+                .buttonStyle(DandiButtonWatch())
+                .padding(.top, 22)
+            }
+            .padding(.horizontal, 40)
+        }
+    }
+//}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Habits.self, configurations: config)
+        return SuggestionWatch()
+            .modelContainer(container)
+    } catch {
+        fatalError("Alguém me desconfigurou")
+    }
+}
