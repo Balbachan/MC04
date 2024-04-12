3e843b4f67c56b3f2672bedb415ad137ea462ee3
Igor Bragança <igor.braganca2003@gmail.com>
2024-04-12 16:46:25 -0300

    Mandando para a bia

--- DandieWatch Watch App/TimePickerWatch .swift
+++ DandieWatch Watch App/TimePickerWatch .swift
@@ -9,9 +9,41 @@ import Foundation
 import SwiftUI
 
 struct TimePickerWatch: View {
+    
+    @State var hours: Int = 0
+    @State var minutes: [Int] = [00, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
+    
     var body: some View {
-        VStack{
-            Text("Hello")
+        GeometryReader { geometry in
+            VStack{
+                Text("Horários")
+                    .font(.system(size: 22))
+                    .fontWeight(.black)
+                    .foregroundStyle(Color.black)
+                
+                //Picker de horas
+                HStack {
+                    Picker("", selection: $hours){
+                        ForEach(0..<24, id: \.self) { i in
+                            Text("\(i)").tag(i)
+                                .font(.custom(FontType.t3.font, size: FontType.t3.rawValue))
+                        }
+                    }.pickerStyle(WheelPickerStyle())
+                    
+                    Text(":")
+                        .font(.custom(FontType.t3.font, size: FontType.t3.rawValue))
+                    
+                    Picker("", selection: $minutes){
+                        ForEach(minutes, id: \.self) { i in
+                            Text("\(i)").tag(i)
+                                .font(.custom(FontType.t3.font, size: FontType.t3.rawValue))
+                        }
+                    }.pickerStyle(WheelPickerStyle())
+                }.frame(width: 180, height: 90)
+                
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .background(.appWhite)
         }
     }
 }
