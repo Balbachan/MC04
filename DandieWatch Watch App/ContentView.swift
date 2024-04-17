a4d6c00ff42f0b96bc3b9ad2a7676237725248fb
Igor Bragança <igor.braganca2003@gmail.com>
2024-04-17 14:14:15 -0300

    Tirando as targets do watch

---
+++ DandieWatch Watch App/ContentView.swift
@@ -0,0 +1,28 @@
+////
+////  ContentView.swift
+////  DandieWatch Watch App
+////
+////  Created by Beatriz Andreucci on 11/04/24.
+////
+//
+//import SwiftUI
+////import SwiftData
+//
+//struct ContentView: View {
+////    @State private var weekModel: WeekModel
+////    
+////    init(modelContext: ModelContext) {
+////        let weekModel = WeekModel(modelContext: modelContext)
+////        _weekModel = State(initialValue: weekModel)
+////    }
+//    
+//    var body: some View {
+//        CalendarWatch(phrases: ["aaa"])
+//            .environmentObject(weekModel)
+////        Text("w")
+//    }
+//}
+//
+////#Preview {
+////    ContentView(modelContext: try! ModelContainer(for: Habit.self).mainContext)
+////}
