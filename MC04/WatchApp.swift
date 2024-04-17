f7fbb2a464df8caafeea0d2f368b3f9526531183
Igor Bragança <igor.braganca2003@gmail.com>
2024-04-17 18:04:18 -0300

    Arrumando o meu cel

--- MC04/WatchApp.swift
+++ MC04/WatchApp.swift
@@ -5,4 +5,16 @@
 //  Created by Beatriz Andreucci on 17/04/24.
 //
 
-import Foundation
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Hi")
+        }
+    }
+}
+
