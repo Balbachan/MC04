39a7baa9b16d3940caa0e7052be31db7149e71e5
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-04-18 14:22:44 -0300

    Merge remote-tracking branch 'refs/remotes/origin/refatoring'
    Conflicts:
    	MC04.xcodeproj/project.pbxproj
    	MC04/Components/WeekHeader.swift

---
+++ MC04/ViewModel/OnboardingModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingModel.swift
+//  Dandie
+//
+//  Created by Igor Bragança Toledo on 17/04/24.
+//
+
+import Foundation
+
+struct OnboardingModel: Observable {
+    
+    let introduction: [String] = ["Prazer! Meu nome é Dandie e meu objetivo\naqui é te auxiliar a ter uma rotina de\nautocuidado.", "Nessa tela você poderá: \nAcompanhar sua rotina\nAdicionar novas tarefas\nVer suas tarefas do dia", "Ao escolher alguma dessas tarefas que fiz\npara você, basta definir o(s) dia(s), se ela irá\nse repetir e o horário que irá realizá-la.", "Viu como é fácil?\n Eu estarei aqui acompanhando seu\nprogresso, além de te lembrar de fazer as\ntarefas, seguir sua rotina com excelência\ne te dar dicas."]
+    
+    let introImagesLight: [String] = ["dandie1", "dandie2", "dandie3", "dandie4"]
+}
---
+++ MC04/WatchApp.swift
@@ -0,0 +1,20 @@
+//
+//  WatchApp.swift
+//  Dandie
+//
+//  Created by Beatriz Andreucci on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Hi")
+        }
+    }
+}
+
