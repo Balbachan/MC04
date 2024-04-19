379ba0ab9d8191eb48ca776fa75a2ae6eb0cd3ee
Igor Bragança <igor.braganca2003@gmail.com>
2024-04-19 17:47:15 -0300

    Enviando para a Laura

--- DandieWatch Watch App/ContentView.swift
+++ DandieWatch Watch App/ContentView.swift
@@ -5,24 +5,36 @@
 //  Created by Beatriz Andreucci on 11/04/24.
 //
 
+import SwiftData
 import SwiftUI
-//import SwiftData
+import UserNotifications
 
 struct ContentView: View {
     @State private var weekModel: WeekModel
-//    
-//    init(modelContext: ModelContext) {
-//        let weekModel = WeekModel(modelContext: modelContext)
-//        _weekModel = State(initialValue: weekModel)
-//    }
+    
+    init(modelContext: ModelContext) {
+        let weekModel = WeekModel(modelContext: modelContext)
+        _weekModel = State(initialValue: weekModel)
+    }
     
     var body: some View {
-        CalendarWatch(phrases: ["aaa"])
-            .environmentObject(weekModel)
-//        Text("w")
+        NavigationStack {
+            VStack {
+                CalendarWatch()
+            }.onAppear{
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("Permission approved!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
+        .environmentObject(weekModel)
     }
 }
 
-//#Preview {
-//    ContentView(modelContext: try! ModelContainer(for: Habit.self).mainContext)
-//}
+#Preview {
+    ContentView(modelContext: try! ModelContainer(for: Habit.self).mainContext)
+}
